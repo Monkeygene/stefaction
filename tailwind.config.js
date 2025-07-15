ffe20b4aa71bff8779240db21b05692cfa847333
Nyoni Safaris <monkeygenesafaris@gmail.com>
2025-07-15 22:04:59 +0100

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
-module.exports = {
-  content: ["./docs/**/*.{html,js}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+  "./index.html",
+  "./portfolio/index.html",
+  "./about/index.html",
+  "./contact/index.html",
+  "./blog/index.html",
+  "./services/index.html",
+  "./src/**/*.{js,ts,jsx,tsx}"],
+   darkMode: 'class', 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        linen: 'rgb(254, 252, 250)',
+      },
+      fontFamily: {
+        londrina: ['LondrinaOutline', 'system-ui', 'sans-serif'],
+         ttfors: ['TTForsOutline', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        portfolio: path.resolve(__dirname, 'portfolio/index.html'),
+        about: path.resolve(__dirname, 'about/index.html'),
+        contact: path.resolve(__dirname, 'contact/index.html'),
+        blog: path.resolve(__dirname, 'blog/index.html'),
+        services: path.resolve(__dirname, 'services/index.html'),
+      }
+    }
+  }
+})
