42d325030ccde67ee21ced2265a55bdf4451ad84
Nyoni Safaris <monkeygenesafaris@gmail.com>
2025-07-16 05:52:25 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/stefaction/',
+  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
